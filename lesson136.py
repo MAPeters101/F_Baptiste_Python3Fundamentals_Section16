47f3099108e9223a3ad3e338c5e2974f17416cf9
Mark Peters <mapeters101@earthlink.net>
2025-03-19 14:51:41 -0400

    Complete work on lesson136 from 2:10.

--- lesson136.py
+++ lesson136.py
@@ -5,6 +5,15 @@ print(lengths)
 lengths = (len(element) for element in data)
 print(lengths)
 print(3 in lengths)
+print('-'*80)
 
+def my_len(x):
+    return len(x)
 
+lengths = (my_len(element) for element in data)
+print(list(lengths))
 
+lengths = map(len, data)
+print(lengths)
+print(list(lengths))
+print(list(lengths))
